b1c4b9c4b30c196204e337b53595ae45de58dd33
MakerSquare Student <jdaudier@outlook.com>
2013-06-24 17:00:02 -0500

    started twilio2 app

--- twilio2.rb
+++ twilio2.rb
@@ -2,11 +2,11 @@ require 'rubygems'
 require 'twilio-ruby'
 require 'sinatra'
 
-account_sid = 'AC0f8c59269730d61d7d83cf182968a50a'
-auth_token = '557cd61e8d61e32aec182e0dec900b2f'
+account_sid = ENV['ACCOUNT_SID'] 
+auth_token = ENV['AUTH_TOKEN']
 client = Twilio::REST::Client.new account_sid, auth_token
 
-get '/sms-quickstart' do
+get '/twilio2' do
   twiml = Twilio::TwiML::Response.new do |r|
     r.Sms "Hey Monkey. Thanks for the message!"
   end
