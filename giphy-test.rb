b8ea4ac02e802518ed6c3181b986de6749f3cbc6
MakerSquare Student <student@challenge-4.att.net>
2013-06-25 17:50:45 -0500

    removed giphy-test.rb file

--- giphy-test.rb
+++
@@ -1,9 +0,0 @@
-require 'net/http'
-require 'json'
-require 'sinatra'
-require "awesome_print"
-
-url = "http://api.giphy.com/v1/gifs/search?q=ryan-gosling&api_key=dc6zaTOxFJmzC&limit=1"
-resp = Net::HTTP.get_response(URI.parse(url))
-buffer = resp.body
-ap result = JSON.parse(buffer)["data"][0]["bitly_gif_url"]
