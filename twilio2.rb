90b4bcfaa7da28a9b1870f2590f87550e6f54c53
MakerSquare Student <student@challenge-4.att.net>
2013-06-25 17:20:07 -0500

    app can now send a link to an animated gif based on your search term

--- twilio2.rb
+++ twilio2.rb
@@ -1,6 +1,9 @@
 require 'rubygems'
 require 'twilio-ruby'
 require 'sinatra'
+require 'net/http'
+require 'json'
+# require "awesome_print"
 
 #Make sure local port matches local tunnel port 8000
 #Make sure local server is running before creating a local tunnel 
@@ -12,8 +15,19 @@ client = Twilio::REST::Client.new account_sid, auth_token
 set :port, 8000
 
 get '/twilio2' do
+  search_term = params[:Body] 
+  #App won't work unless you pass in the search params. 
+  #For instance: http://58x5.localtunnel.com/twilio2?Body=channing 
+  
+  return "Please specify a search term." if params[:Body].nil?
+
+  url = "http://api.giphy.com/v1/gifs/search?q=#{search_term.gsub("\n", '')}&api_key=dc6zaTOxFJmzC&limit=1"
+  resp = Net::HTTP.get_response(URI.parse(url))
+  buffer = resp.body
+  result = JSON.parse(buffer)["data"][0]["bitly_gif_url"]
   twiml = Twilio::TwiML::Response.new do |r|
-    r.Sms "Hey Monkey. Thanks for the message!"
+    r.Sms "Click the link for your animated gif! #{result}"
   end
   twiml.text
 end
+
