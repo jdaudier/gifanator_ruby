4c4bd6605d76f32e7b60834333be2e9b337326fa
MakerSquare Student <student@challenge-4.att.net>
2013-06-25 14:59:51 -0500

    got local tunnel working and changed the port to 8000

--- twilio2.rb
+++ twilio2.rb
@@ -2,9 +2,14 @@ require 'rubygems'
 require 'twilio-ruby'
 require 'sinatra'
 
+#Make sure local port matches local tunnel port 8000
+#Make sure local server is running before creating a local tunnel 
+
 account_sid = ENV['ACCOUNT_SID'] 
 auth_token = ENV['AUTH_TOKEN']
 client = Twilio::REST::Client.new account_sid, auth_token
+
+set :port, 8000
 
 get '/twilio2' do
   twiml = Twilio::TwiML::Response.new do |r|
