103b425d27effe276bab71fdbcb03c1b8b270559
Joanne Daudier <joanne_hwang1223@hotmail.com>
2013-06-26 00:12:05 -0500

    cleaned up comments on how to use local tunnel

--- twilio2.rb
+++ twilio2.rb
@@ -5,14 +5,14 @@ require 'net/http'
 require 'json'
 # require "awesome_print"
 
+#If using local tunnel
+# set :port, 8000
 #Make sure local port matches local tunnel port 8000
 #Make sure local server is running before creating a local tunnel 
 
 account_sid = ENV['ACCOUNT_SID'] 
 auth_token = ENV['AUTH_TOKEN']
 client = Twilio::REST::Client.new account_sid, auth_token
-
-# set :port, 8000
 
 get '/twilio2' do
   search_term = params[:Body] 
