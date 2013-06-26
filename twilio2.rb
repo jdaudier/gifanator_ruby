1b7d0d072f54017febf6d48e200333587d25ef03
MakerSquare Student <student@macintosh.att.net>
2013-06-26 15:40:41 -0500

    updated error message

--- twilio2.rb
+++ twilio2.rb
@@ -19,7 +19,7 @@ get '/twilio2' do
   #App won't work unless you pass in the search params. 
   #For instance: http://58x5.localtunnel.com/twilio2?Body=channing 
   
-#   return "Please specify a search term." if params[:Body].nil?
+  return "Send a text message to 858-224-9485 and specify a search term." if params[:Body].nil?
 
 #   url = "http://api.giphy.com/v1/gifs/search?q=#{search_term.gsub(' ', '-')}&api_key=dc6zaTOxFJmzC&limit=1"
 #   resp = Net::HTTP.get_response(URI.parse(url))
