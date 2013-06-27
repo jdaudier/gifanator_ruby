651b434703f2ffc77de913e457f103cc5698b531
Joanne Daudier <joanne_hwang1223@hotmail.com>
2013-06-27 17:42:03 -0500

    added funner messages

--- twilio2.rb
+++ twilio2.rb
@@ -11,13 +11,13 @@ client = Twilio::REST::Client.new account_sid, auth_token
 
 #Scenario 1: User text "random" or "Random"
 #Scenario 2: If there's a 10-digit phone # in the text msg, store that #, and send gif URL to that number.
-#Scenario 3:If there's no number in the text msg, send gif to user.
+#Scenario 3: If there's no number in the text msg, send gif to user.
 
 get '/twilio2' do
 #App won't work unless you pass in the search params. 
 #For instance: http://58x5.localtunnel.com/twilio2?Body=channing 
 
-return "Send a text message to 858-224-9485 and specify a search term." if params[:Body].nil?
+return "Send a text message to 858-224-9485 and specify a search term. Text 'random' if you want a random gif. Add a number in your text to send to a friend." if params[:Body].nil?
 
 search_term = params[:Body].downcase
 sender = params[:From]
@@ -36,7 +36,7 @@ result = JSON.parse(buffer)["data"]["bitly_gif_url"]
     id = JSON.parse(buffer)["data"]["id"]
     result = "http://giphy.com/gifs/#{id}"
     twiml = Twilio::TwiML::Response.new do |r|
-      r.Sms "Here's a random animated gif! #{result}"
+      r.Sms "Confucius says: Man who text number, gets random animated gif! #{result}"
     end
     twiml.text
   elsif friends_number != "" #if friend's number is not blank
@@ -45,12 +45,12 @@ result = JSON.parse(buffer)["data"]["bitly_gif_url"]
         :from => "+18582249485")
     puts message.sid
     twiml = Twilio::TwiML::Response.new do |r|
-      r.Sms "We've just sent your friend this awesome animated gif! #{result}"
+      r.Sms "BOOM! We've just sent your friend this awesome animated gif! #{result}"
     end
     twiml.text
   else #if there is no number
     twiml = Twilio::TwiML::Response.new do |r|
-      r.Sms "Click the link for your animated gif! #{result}"
+      r.Sms "Click the link for your totally awesome animated gif. Booyah! #{result}"
     end
     twiml.text
   end
