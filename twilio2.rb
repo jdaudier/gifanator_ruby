e322147cf0a5375be961d158ae00656886b76bf1
MakerSquare Student <student@Macintosh.local>
2013-06-26 21:35:59 -0500

    more testing

--- twilio2.rb
+++ twilio2.rb
@@ -33,8 +33,8 @@ friends_number = search_term.match(/\d{10}/).to_s #Extract phone # and turns it 
     url = "http://api.giphy.com/v1/gifs/search?q=#{search_term.gsub('friends_number', '').gsub(' ', '-')}&api_key=dc6zaTOxFJmzC&limit=1"
     resp = Net::HTTP.get_response(URI.parse(url))
     buffer = resp.body
-    result = JSON.parse(buffer)["data"]["bitly_gif_url"]
-    message = client.account.sms.messages.create(:body => "Your friend just sent you an animated gif! #{result}",
+    final_link = JSON.parse(buffer)["data"][0]["bitly_gif_url"]
+    message = client.account.sms.messages.create(:body => "Your friend just sent you an animated gif! #{final_link}",
         :to => friends_number,
         :from => "+18582249485")
     puts message.sid
