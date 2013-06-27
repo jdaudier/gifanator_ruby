2940d9411d0dbf3863a36fe59272072190539a6e
MakerSquare Student <student@Macintosh.local>
2013-06-26 21:55:36 -0500

    removed local tunnel notes

--- twilio2.rb
+++ twilio2.rb
@@ -5,11 +5,6 @@ require 'net/http'
 require 'json'
 # require "awesome_print"
 
-#If using local tunnel
-# set :port, 8000
-#Make sure local port matches local tunnel port 8000
-#Make sure local server is running before creating a local tunnel 
-
 account_sid = ENV['ACCOUNT_SID'] 
 auth_token = ENV['AUTH_TOKEN']
 client = Twilio::REST::Client.new account_sid, auth_token
