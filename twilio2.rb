433580882b98bfc4618a73882510b7070c5e6e0f
MakerSquare Student <student@Macintosh.local>
2013-06-26 20:44:12 -0500

    more testing

--- twilio2.rb
+++ twilio2.rb
@@ -37,7 +37,7 @@ friends_number = search_term.match(/\d{10}/).to_s #Extract phone # and turns it 
     message = client.account.sms.messages.create(:body => "Your friend just sent you an animated gif! #{result}",
         :to => friends_number,
         :from => "+18582249485")
-    puts message.body
+    puts message.sid
   else #if there is no number
     url = "http://api.giphy.com/v1/gifs/search?q=#{search_term.gsub('friends_number', '').gsub(' ', '-')}&api_key=dc6zaTOxFJmzC&limit=1"
     resp = Net::HTTP.get_response(URI.parse(url))
