c5e8f72f372a4ae4e97c36d2660c0b7b5da6e46d
MakerSquare Student <student@themakersquare.com>
2013-06-27 16:08:56 -0500

    getting back larger random gif

--- twilio2.rb
+++ twilio2.rb
@@ -33,7 +33,7 @@ result = JSON.parse(buffer)["data"]["bitly_gif_url"]
     url = "http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC"
     resp = Net::HTTP.get_response(URI.parse(url))
     buffer = resp.body
-    result = JSON.parse(buffer)["data"]["image_original_url"]
+    result = JSON.parse(buffer)["data"]["image_original_url"].gsub("media.giphy.com/media", "giphy.com/gifs").gsub("giphy.gif", "")
     twiml = Twilio::TwiML::Response.new do |r|
       r.Sms "Here's a random animated gif! #{result}"
     end
