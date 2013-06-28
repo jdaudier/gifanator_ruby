e8cfad108cd4d4bd06e563bd6a680a52ede8a210
Joanne Daudier <joanne_hwang1223@hotmail.com>
2013-06-28 18:40:11 -0500

    moved if statement for no data up

--- twilio2.rb
+++ twilio2.rb
@@ -27,15 +27,6 @@ get '/twilio2' do
   url = "http://api.giphy.com/v1/gifs/translate?s=#{search_term.gsub(friends_number, '').gsub(' ', '+')}&api_key=dc6zaTOxFJmzC&limit=1"
   resp = Net::HTTP.get_response(URI.parse(url))
   buffer = resp.body
-  result = JSON.parse(buffer)["data"]["bitly_gif_url"]
-
-  def random
-      url = "http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC"
-      resp = Net::HTTP.get_response(URI.parse(url))
-      buffer = resp.body
-      id = JSON.parse(buffer)["data"]["id"]
-      "http://giphy.com/gifs/#{id}"
-  end
 
   def sendtext(reply)
     twiml = Twilio::TwiML::Response.new do |r|
@@ -46,6 +37,16 @@ get '/twilio2' do
 
   if JSON.parse(buffer)["data"] == []
     sendtext("What what? Who would search for that? Sorry, no results found! http://gph.is/XIjPNh")
+  else
+      result = JSON.parse(buffer)["data"]["bitly_gif_url"]
+  end
+
+  def random
+      url = "http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC"
+      resp = Net::HTTP.get_response(URI.parse(url))
+      buffer = resp.body
+      id = JSON.parse(buffer)["data"]["id"]
+      "http://giphy.com/gifs/#{id}"
   end
 
   if search_term == "random"
