66c0055501b15976e8610fa34488fbf7d9c7553d
Joanne Daudier <joanne_hwang1223@hotmail.com>
2013-06-28 15:02:37 -0500

    fixed conditional bug

--- twilio2.rb
+++ twilio2.rb
@@ -48,14 +48,14 @@ get '/twilio2' do
     sendtext("Confucius says: Man who text me, gets random animated gif! #{random}")
 
   elsif friends_number != "" #if friend's number is not blank
-    if search_term == "random"
+    if search_term.include? "random"
     result = random
-    message = client.account.sms.messages.create(:body => "Your friend at this number #{sender} just sent you a random animated gif! #{result}",
+    message = client.account.sms.messages.create(:body => "Your friend at this number #{sender} just sent you a RANDOM animated gif! #{result}",
         :to => friends_number,
         :from => "+18582249485")
     puts message.sid
     
-    sendtext("BOOM! We've just sent your friend this awesome random animated gif! #{result}")
+    sendtext("BOOM! We've just sent your friend this awesome RANDOM animated gif! #{result}")
     else
     message = client.account.sms.messages.create(:body => "Your friend at this number #{sender} just sent you an animated gif! #{result}",
         :to => friends_number,
