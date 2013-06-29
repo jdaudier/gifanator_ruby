2163b5e02b4642b0cb4e80dc8352d90611aac6b1
Joanne Daudier <joanne_hwang1223@hotmail.com>
2013-06-28 21:47:28 -0500

    changed to send msg method

--- twilio2.rb
+++ twilio2.rb
@@ -27,8 +27,16 @@ get '/twilio2' do
   url = "http://api.giphy.com/v1/gifs/translate?s=#{search_term.gsub(friends_number, '').gsub(' ', '+')}&api_key=dc6zaTOxFJmzC&limit=1"
   resp = Net::HTTP.get_response(URI.parse(url))
   buffer = resp.body
-  result = JSON.parse(buffer)["data"]["bitly_gif_url"]
 
+  if JSON.parse(buffer)["data"].empty?
+    message = client.account.sms.messages.create(:body => "What what? Who would search for that? Sorry, no results found! http://gph.is/XIjPNh",
+        :to => sender,
+        :from => "+18582249485")
+    puts message.sid
+  else
+    result = JSON.parse(buffer)["data"]["bitly_gif_url"]
+  end
+  
   def sendtext(reply)
     twiml = Twilio::TwiML::Response.new do |r|
       r.Sms(reply)
@@ -64,12 +72,6 @@ get '/twilio2' do
     
     sendtext("BOOM! We've just sent your friend this awesome animated gif! #{result}")
     end
-  
-  elsif JSON.parse(buffer)["data"].empty?
-    message = client.account.sms.messages.create(:body => "What what? Who would search for that? Sorry, no results found! http://gph.is/XIjPNh",
-        :to => sender,
-        :from => "+18582249485")
-    puts message.sid
 
   else #if there is no number
     sendtext("Click the link for your totally awesome animated gif. Booyah! #{result}")
